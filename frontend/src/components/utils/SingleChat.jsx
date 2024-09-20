f977e8c79a8e6d954f052cd556c01aaf2281f32d
Paras Malhotra <37955292+parasmalhotra522@users.noreply.github.com>
2024-09-20 11:51:09 -0600

    Update SingleChat.jsx

--- frontend/src/components/utils/SingleChat.jsx
+++ frontend/src/components/utils/SingleChat.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { ChatState } from '../../context/ChatProvider';
 import { Box, Spinner, Text, FormControl, Input, useToast } from '@chakra-ui/react';
